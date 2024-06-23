74e46ef9459ec2cf628a7d4fc03d4d91e15ee572
Brandon Cubillos <brandon.cubillos.19@alumnos.uda.cl>
2024-06-23 00:26:50 -0400

    Progreso: Se descargaron las depencias de bootstrap y se crea la primera pagina del proyecto (tienda.php).

---
+++ tienda.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Bootstrap/styles.css">
+</head>
+<body class="bg-dark text-light">
+    <nav class="navbar "><span>Hola como estai</span></nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
