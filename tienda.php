ac89bab335d9a8058e54d7cb204821b960d1ff92
Brandon Cubillos <brandon.cubillos.19@alumnos.uda.cl>
2024-07-06 20:18:30 -0400

    Se termina el formato general de la TIENDA de la pagina web (colores y estilos de la pagina se encuentran sujetos a cambios). Se crea carpeta Images para guardar momentaneamente diferentes tipos de imagenes de la pagina. Se crea una nueva hoja de estilos para personalizar aun mas la pagina.

--- tienda.php
+++ tienda.php
@@ -1,13 +1,204 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ancient</title>
+
     <link rel="stylesheet" href="Bootstrap/styles.css">
+    <link rel="stylesheet" href="Bootstrap/stylesPersonalizados.css">
+    
 </head>
-<body class="bg-dark text-light">
-    <nav class="navbar "><span>Hola como estai</span></nav>
+<!-- cuerpo: bg-dark -->
+<body class="text-light" style="background-color:#18181C">
+    <div class="container">
+        <nav class="navbar navbar-dark navbar-expand-lg body-tertiary p-4">
+            <div class="container-fluid">
+                <span class="navbar-brand h1">Ancient</span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!-- Mejorar con PHP el resalte de la fuente, dependiendo en qué página nos encontremos -->
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">TIENDA</a> 
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="nosotros.php">ACERCA DE NOSOTROS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SOPORTE</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#"></a>
+                        </li>
+                    </ul>
+                    <a class="nav-link text-white-50 p-1" href="#">INICIAR SESION</a>
+                    <a class="nav-link text-white-50 p-1" href="#">REGISTRARSE</a>
+                </div>
+            </div> 
+        </nav>
+    </div>
+    <!-- beige/bisque/blanchedalmond -->
+    <div class="container-fluid mb-4 p-0" style="background-color:#121212">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 bannerContainer bg-black">
+                    <a href="..."><img class="d-block w-100 m-0 p-0 bannerImage" src="Images/elden-ring-banner.png" alt="Elden Ring DLC"></a>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Carrusel de juegos 1 -->
+        <div class="container-fluid my-5">
+            <div class="container">
+                <div class="row m-0 p-0">
+                    <div class="col m-0 p-0">
+                        <div id="carousel-1" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carousel-1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Juego 1"></button>
+                                <button type="button" data-bs-target="#carousel-1" data-bs-slide-to="1" aria-label="Juego 2"></button>
+                                <button type="button" data-bs-target="#carousel-1" data-bs-slide-to="2" aria-label="Juego 3"></button>
+                                <button type="button" data-bs-target="#carousel-1" data-bs-slide-to="3" aria-label="Juego 4"></button>
+                                <button type="button" data-bs-target="#carousel-1" data-bs-slide-to="4" aria-label="Juego 5"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active c-1-item">
+                                    <img src="Images/counter-strike-2-carousel.jpeg" class="d-block w-100 c-1-img" alt="...">
+                                </div>
+                                <div class="carousel-item c-1-item">
+                                    <img src="Images/rdr-2-4k-carousel.png" class="d-block w-100 c-1-img" alt="...">
+                                </div>
+                                <div class="carousel-item c-1-item">
+                                    <img src="Images/sea-arreglado-carousel.png" class="d-block w-100 c-1-img" alt="...">
+                                </div>
+                                <div class="carousel-item c-1-item">
+                                    <img src="Images/metroid-dread-carousel.png" class="d-block w-100 c-1-img" alt="...">
+                                </div>
+                                <div class="carousel-item c-1-item">
+                                    <img src="Images/baldurs-gate-3-carousel.jpg" class="d-block w-100 c-1-img" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carousel-1" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carousel-1" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Conjunto de juegos -->
+        <div class="container-fluid mb-5">
+            <div class="container" >
+                <div class="row">
+                    <div class="col-3">
+                        <img src="Images/farcry-6-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">80%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 45.000</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 9.000</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/re-4-remake-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">35%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 35.750</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 23.238</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/fallout-76-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">70%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 26.499</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 7.950</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/stanley-parable-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">50%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 9.500</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 4.750</span>
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col-4">
+                        <img src="Images/outer-wilds-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">40%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 12.500</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 7.500</span>
+                    </div>
+                    <div class="col-4">
+                        <img src="Images/assassin-valhalla-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">80%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 44.900</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 8.980</span>
+                    </div>
+                    <div class="col-4">
+                        <img src="Images/witcher-3-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">90%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 18.300</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 1.830</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <img src="Images/sekiro-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">50%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 47.650</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 23.825</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/gta-5-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">65%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 29.900</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 10.465</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/stardew-valley-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">40%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 7.500</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 4.500</span>
+                    </div>
+                    <div class="col-3">
+                        <img src="Images/hogwarts-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">60%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 39.999</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 16.000</span>
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col-4">
+                        <img src="Images/inscryption-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">60%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 9.000</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 3.600</span>
+                    </div>
+                    <div class="col-4">
+                        <img src="Images/hades-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">60%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 13.000</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 5.200</span>
+                    </div>
+                    <div class="col-4">
+                        <img src="Images/fallout-4-container-2.jpg" class="img-fluid">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">60%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 13.900</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 5.560</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Grid de juegos 2 (tam imagen: 616x353px) -->
+
+        <div class="container-fluid my-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1593500/capsule_616x353.jpg?t=1717621786">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">50%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 35.000</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 17.500</span>
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/945360/capsule_616x353.jpg?t=1718741655">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">40%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 2.499</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 1.499</span>
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1222140/capsule_616x353.jpg?t=1667468479">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">60%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 24.900</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 9.960</span>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/447040/capsule_616x353_spanish.jpg?t=1692817739">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">90%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 34.900</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 3.490</span>
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/728880/capsule_616x353.jpg?t=1718623620">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">75%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 12.990</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 3.248</span>
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100 m-0 p-0" src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2138710/capsule_616x353.jpg?t=1718182149">
+                        <span class="fs-5 ps-2 pb-1 pe-1 bg-warning text-dark text-center border border-black">50%</span><span class="fs-5 ps-1 pb-1 text-warning-emphasis text-decoration-line-through bg-dark border border-black border-start-0 border-end-0">CLP$ 14.030</span><span class="fs-5 pb-1 pe-1 ps-1 bg-dark border border-black border-start-0">CLP$ 7.015</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
